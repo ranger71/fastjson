5bdb32387bfbb2db491a73485cc8b4717282b231
Rani Ettinger <ran.ettinger@gmail.com>
2017-09-22 18:42:54 +0100

    Re-extracting clone 7 into a method object.

--- src/main/java/com/alibaba/fastjson/parser/JSONLexer.java
+++ src/main/java/com/alibaba/fastjson/parser/JSONLexer.java
@@ -3177,138 +3177,12 @@ public final class JSONLexer {
                 float[] array = new float[16];
                 int arrayIndex = 0;
 
-                for (; ; ) {
-                    int start = bp + offset - 1;
-                    boolean negative = chLocal == '-';
-                    if (negative) {
-                        // chLocal = charAt(bp + (offset++));
-                        charIndex = bp + (offset++);
-                        chLocal = charIndex >= this.len ? //
-                                EOI //
-                                : text.charAt(charIndex);
-                    }
-
-                    if (chLocal >= '0' && chLocal <= '9') {
-                        int intVal = chLocal - '0';
-                        for (; ; ) {
-                            // chLocal = charAt(bp + (offset++));
-                            charIndex = bp + (offset++);
-                            chLocal = charIndex >= this.len ? //
-                                    EOI //
-                                    : text.charAt(charIndex);
-
-                            if (chLocal >= '0' && chLocal <= '9') {
-                                intVal = intVal * 10 + (chLocal - '0');
-                                continue;
-                            } else {
-                                break;
-                            }
-                        }
-
-                        int power = 1;
-                        if (chLocal == '.') {
-                            // chLocal = charAt(bp + (offset++));
-                            charIndex = bp + (offset++);
-                            chLocal = charIndex >= this.len ? //
-                                    EOI //
-                                    : text.charAt(charIndex);
-
-                            if (chLocal >= '0' && chLocal <= '9') {
-                                intVal = intVal * 10 + (chLocal - '0');
-                                power *= 10;
-                                for (; ; ) {
-                                    // chLocal = charAt(bp + (offset++));
-                                    charIndex = bp + (offset++);
-                                    chLocal = charIndex >= this.len ? //
-                                            EOI //
-                                            : text.charAt(charIndex);
-
-                                    if (chLocal >= '0' && chLocal <= '9') {
-                                        intVal = intVal * 10 + (chLocal - '0');
-                                        power *= 10;
-                                        continue;
-                                    } else {
-                                        break;
-                                    }
-                                }
-                            } else {
-                                matchStat = NOT_MATCH;
-                                return null;
-                            }
-                        }
-
-                        boolean exp = chLocal == 'e' || chLocal == 'E';
-                        if (exp) {
-                            // chLocal = charAt(bp + (offset++));
-                            charIndex = bp + (offset++);
-                            chLocal = charIndex >= this.len ? //
-                                    EOI //
-                                    : text.charAt(charIndex);
-                            if (chLocal == '+' || chLocal == '-') {
-                                // chLocal = charAt(bp + (offset++));
-                                charIndex = bp + (offset++);
-                                chLocal = charIndex >= this.len ? //
-                                        EOI //
-                                        : text.charAt(charIndex);
-                            }
-                            for (;;) {
-                                if (chLocal >= '0' && chLocal <= '9') {
-                                    // chLocal = charAt(bp + (offset++));
-                                    charIndex = bp + (offset++);
-                                    chLocal = charIndex >= this.len ? //
-                                            EOI //
-                                            : text.charAt(charIndex);
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-
-                        int count = bp + offset - start - 1;
-                        float value;
-                        if (!exp && count < 10) {
-                            value = ((float) intVal) / power;
-                            if (negative) {
-                                value = -value;
-                            }
-                        } else {
-                            String text = this.subString(start, count);
-                            value = Float.parseFloat(text);
-                        }
-
-                        if (arrayIndex >= array.length) {
-                            float[] tmp = new float[array.length * 3 / 2];
-                            System.arraycopy(array, 0, tmp, 0, arrayIndex);
-                            array = tmp;
-                        }
-                        array[arrayIndex++] = value;
-
-                        if (chLocal == ',') {
-                            // chLocal = charAt(bp + (offset++));
-                            charIndex = bp + (offset++);
-                            chLocal = charIndex >= this.len ? //
-                                    EOI //
-                                    : text.charAt(charIndex);
-                        } else if (chLocal == ']') {
-                            // chLocal = charAt(bp + (offset++));
-                            charIndex = bp + (offset++);
-                            chLocal = charIndex >= this.len ? //
-                                    EOI //
-                                    : text.charAt(charIndex);
-                            break;
-                        }
-                    } else {
-                        matchStat = NOT_MATCH;
-                        return null;
-                    }
-                }
-
-                // compact
-                if (arrayIndex != array.length) {
-                    float[] tmp = new float[arrayIndex];
-                    System.arraycopy(array, 0, tmp, 0, arrayIndex);
-                    array = tmp;
-                }
+                Clone7 clone7 = new Clone7(offset, chLocal, array, arrayIndex).invoke();
+                if (clone7.is()) return null;
+                array = clone7.getArray();
+                arrayIndex = clone7.getArrayIndex();
+                chLocal = clone7.getChLocal();
+                offset = clone7.getOffset();
 
                 if (arrayarrayIndex >= arrayarray.length) {
                     float[][] tmp = new float[arrayarray.length * 3 / 2][];
@@ -4962,6 +4836,181 @@ public final class JSONLexer {
             }
 
 
+            if (arrayIndex != array.length) {
+                float[] tmp = new float[arrayIndex];
+                System.arraycopy(array, 0, tmp, 0, arrayIndex);
+                array = tmp;
+            }
+            myResult = false;
+            return this;
+        }
+    }
+
+    private class Clone7 {
+        private boolean myResult;
+        private int offset;
+        private char chLocal;
+        private float[] array;
+        private int arrayIndex;
+
+        public Clone7(int offset, char chLocal, float[] array, int arrayIndex) {
+            this.offset = offset;
+            this.chLocal = chLocal;
+            this.array = array;
+            this.arrayIndex = arrayIndex;
+        }
+
+        boolean is() {
+            return myResult;
+        }
+
+        public int getOffset() {
+            return offset;
+        }
+
+        public char getChLocal() {
+            return chLocal;
+        }
+
+        public float[] getArray() {
+            return array;
+        }
+
+        public int getArrayIndex() {
+            return arrayIndex;
+        }
+
+        public Clone7 invoke() {
+            int charIndex;
+            for (; ; ) {
+                int start = bp + offset - 1;
+                boolean negative = chLocal == '-';
+                if (negative) {
+                    // chLocal = charAt(bp + (offset++));
+                    charIndex = bp + (offset++);
+                    chLocal = charIndex >= JSONLexer.this.len ? //
+                            EOI //
+                            : text.charAt(charIndex);
+                }
+
+                if (chLocal >= '0' && chLocal <= '9') {
+                    int intVal = chLocal - '0';
+                    for (; ; ) {
+                        // chLocal = charAt(bp + (offset++));
+                        charIndex = bp + (offset++);
+                        chLocal = charIndex >= JSONLexer.this.len ? //
+                                EOI //
+                                : text.charAt(charIndex);
+
+                        if (chLocal >= '0' && chLocal <= '9') {
+                            intVal = intVal * 10 + (chLocal - '0');
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    int power = 1;
+                    if (chLocal == '.') {
+                        // chLocal = charAt(bp + (offset++));
+                        charIndex = bp + (offset++);
+                        chLocal = charIndex >= JSONLexer.this.len ? //
+                                EOI //
+                                : text.charAt(charIndex);
+
+                        if (chLocal >= '0' && chLocal <= '9') {
+                            intVal = intVal * 10 + (chLocal - '0');
+                            power *= 10;
+                            for (; ; ) {
+                                // chLocal = charAt(bp + (offset++));
+                                charIndex = bp + (offset++);
+                                chLocal = charIndex >= JSONLexer.this.len ? //
+                                        EOI //
+                                        : text.charAt(charIndex);
+
+                                if (chLocal >= '0' && chLocal <= '9') {
+                                    intVal = intVal * 10 + (chLocal - '0');
+                                    power *= 10;
+                                    continue;
+                                } else {
+                                    break;
+                                }
+                            }
+                        } else {
+                            matchStat = NOT_MATCH;
+                            myResult = true;
+                            return this;
+                        }
+                    }
+
+                    boolean exp = chLocal == 'e' || chLocal == 'E';
+                    if (exp) {
+                        // chLocal = charAt(bp + (offset++));
+                        charIndex = bp + (offset++);
+                        chLocal = charIndex >= JSONLexer.this.len ? //
+                                EOI //
+                                : text.charAt(charIndex);
+                        if (chLocal == '+' || chLocal == '-') {
+                            // chLocal = charAt(bp + (offset++));
+                            charIndex = bp + (offset++);
+                            chLocal = charIndex >= JSONLexer.this.len ? //
+                                    EOI //
+                                    : text.charAt(charIndex);
+                        }
+                        for (;;) {
+                            if (chLocal >= '0' && chLocal <= '9') {
+                                // chLocal = charAt(bp + (offset++));
+                                charIndex = bp + (offset++);
+                                chLocal = charIndex >= JSONLexer.this.len ? //
+                                        EOI //
+                                        : text.charAt(charIndex);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    int count = bp + offset - start - 1;
+                    float value;
+                    if (!exp && count < 10) {
+                        value = ((float) intVal) / power;
+                        if (negative) {
+                            value = -value;
+                        }
+                    } else {
+                        String text = JSONLexer.this.subString(start, count);
+                        value = Float.parseFloat(text);
+                    }
+
+                    if (arrayIndex >= array.length) {
+                        float[] tmp = new float[array.length * 3 / 2];
+                        System.arraycopy(array, 0, tmp, 0, arrayIndex);
+                        array = tmp;
+                    }
+                    array[arrayIndex++] = value;
+
+                    if (chLocal == ',') {
+                        // chLocal = charAt(bp + (offset++));
+                        charIndex = bp + (offset++);
+                        chLocal = charIndex >= JSONLexer.this.len ? //
+                                EOI //
+                                : text.charAt(charIndex);
+                    } else if (chLocal == ']') {
+                        // chLocal = charAt(bp + (offset++));
+                        charIndex = bp + (offset++);
+                        chLocal = charIndex >= JSONLexer.this.len ? //
+                                EOI //
+                                : text.charAt(charIndex);
+                        break;
+                    }
+                } else {
+                    matchStat = NOT_MATCH;
+                    myResult = true;
+                    return this;
+                }
+            }
+
+            // compact
             if (arrayIndex != array.length) {
                 float[] tmp = new float[arrayIndex];
                 System.arraycopy(array, 0, tmp, 0, arrayIndex);
